a766e12bff438f10722f44d47d49417971957d04
aaronrbg <aaronrbg@aaronrbg.com>
2018-11-26 19:26:03 -0800

    brakes up sample html into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import Nav from './Nav.jsx'
+import MessageList from './MessageList.jsx'
+import ChatBar from './ChatBar.jsx'
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <Fragment>
+        <Nav />
+        <MessageList />
+        <ChatBar />
+      </Fragment>
     );
   }
 }
